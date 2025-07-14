b04129067d012e7af5021d076b6bbcfebb204f24
강 의주 <cupid33@c3r6s7.codyssey.kr>
2025-07-14 22:45:12 +0900

    .

--- README.md
+++ README.md
@@ -1,4 +1,6 @@
 # codyssey
 Github와 codyssey를 연동하기 위한 Repository
 
-changed
+git add .
+git commit -m "."
+git push
--- my_solution.py
+++ my_solution.py
@@ -1,2 +1,5 @@
-def hello():
+def hello() -> str:
     return "Hello"
+
+if __name__ == '__main__':
+    print(hello())
